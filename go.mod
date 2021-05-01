2c00ce36029e0439d8b0d100054bf9921454186a
Alex Komoroske <jkomoros@gmail.com>
2021-05-01 14:57:30 -0700

    Initial commit for a hello world based on https://github.com/bwmarrin/discordgo/blob/master/examples/airhorn/main.go

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jkomoros/gale-x-bot
 
 go 1.16
+
+require github.com/bwmarrin/discordgo v0.23.2
---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+func init() {
+	flag.StringVar(&token, "t", "", "Bot Token")
+	flag.Parse()
+}
+
+const APP_NAME = "gale-x-bot"
+
+var token string
+
+func main() {
+
+	if token == "" {
+		fmt.Println("No token provided. Please run: " + APP_NAME + " -t <bot token>")
+		return
+	}
+
+	// Create a new Discord session using the provided bot token.
+	dg, err := discordgo.New("Bot " + token)
+	if err != nil {
+		fmt.Println("Error creating Discord session: ", err)
+		return
+	}
+
+	// Register ready as a callback for the ready events.
+	dg.AddHandler(ready)
+
+	//TODO: dg.Identify.Intents definition
+
+	// Open the websocket and begin listening.
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("Error opening Discord session: ", err)
+	}
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println(APP_NAME + " is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	dg.Close()
+}
+
+// This function will be called (due to AddHandler above) when the bot receives
+// the "ready" event from Discord.
+func ready(s *discordgo.Session, event *discordgo.Ready) {
+
+	fmt.Println("Hello, world!")
+}
