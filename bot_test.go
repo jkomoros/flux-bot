82ff5c70f61cb070dd813d3d0c051d2088ea4d28
Dimitri Glazkov <dimitri@glazkov.com>
2021-05-15 11:59:32 -0700

    Make the test code path actually follow to happy conclusion.

--- bot_test.go
+++ bot_test.go
@@ -37,15 +37,20 @@ func TestGuildCreate(t *testing.T) {
 	guild := &discordgo.Guild{
 		ID: "guild-1",
 	}
+	// GuildID is necessary in archiveThreadsIfNecessary
+	// TODO: maybe find a way to not need it
 	guild.Channels = append(guild.Channels, &discordgo.Channel{
-		ID:   "channel-1",
-		Type: discordgo.ChannelTypeGuildCategory,
-		Name: THREAD_ARCHIVE_CATEGORY_NAME + " 72",
+		ID:      "channel-1",
+		Type:    discordgo.ChannelTypeGuildCategory,
+		Name:    THREAD_ARCHIVE_CATEGORY_NAME + " 72",
+		GuildID: "guild-1",
 	})
+
 	guild.Channels = append(guild.Channels, &discordgo.Channel{
-		ID:   "channel-2",
-		Type: discordgo.ChannelTypeGuildCategory,
-		Name: THREAD_CATEGORY_NAME,
+		ID:      "channel-2",
+		Type:    discordgo.ChannelTypeGuildCategory,
+		Name:    THREAD_CATEGORY_NAME,
+		GuildID: "guild-1",
 	})
 	guildCreate := newGuildCreate(guild)
 	session.State.GuildAdd(guildCreate.Guild)
