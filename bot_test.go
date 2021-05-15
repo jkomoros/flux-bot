4707d48ac21b0250835c14ff0d4a987f405a0332
Dimitri Glazkov <dimitri@glazkov.com>
2021-05-14 17:56:39 -0700

    Tweak comment.

--- bot_test.go
+++ bot_test.go
@@ -11,7 +11,7 @@ func TestBotInstantiation(t *testing.T) {
 	session := discord.NewSessionTestDriver()
 	newBot(session)
 	// if the assertion below fails, you probably added new API callbacks.
-	// Add them to to test driver to make it pass.
+	// Add them to wrappers.go and test_harness.go to make this test pass.
 	assert.False(t, session.HasUnknownHandlers, "should not register unknown callbacks")
 	// if this assertion fails, bot no longer registers the "ready" callback.
 	assert.True(t, session.InvokeReady(), "ready handler should be set and invokable")
