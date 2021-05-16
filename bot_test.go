737ccd36487b7b2b3a20afe22462b6fd5b2981aa
Dimitri Glazkov <dimitri@glazkov.com>
2021-05-16 12:28:17 -0700

    Add a test for creating archived threads category Part of #32.

--- bot_test.go
+++ bot_test.go
@@ -7,11 +7,12 @@ import (
 )
 
 const (
-	fakeToken string = "fake-value"
+	TEST_TOKEN    string = "fake-value"
+	TEST_GUILD_ID string = "guild-1"
 )
 
 func newSessionStub() *discordgo.Session {
-	session, _ := discordgo.New(fakeToken)
+	session, _ := discordgo.New(TEST_TOKEN)
 	return session
 }
 
@@ -26,13 +27,21 @@ func newGuildCreate(guild *discordgo.Guild) *discordgo.GuildCreate {
 }
 
 type TestController struct {
+	session                            *discordgo.Session
 	channelEditComplexCallCount        int
 	guildChannelCreateComplexCallCount int
 }
 
 func (tc *TestController) GuildChannelCreateComplex(guildID string, data discordgo.GuildChannelCreateData) (st *discordgo.Channel, err error) {
 	tc.guildChannelCreateComplexCallCount++
-	return nil, nil
+	channel := &discordgo.Channel{
+		ID:      "thread-archive-category",
+		Type:    discordgo.ChannelTypeGuildCategory,
+		Name:    THREAD_ARCHIVE_CATEGORY_NAME + " 13",
+		GuildID: TEST_GUILD_ID,
+	}
+	tc.session.State.ChannelAdd(channel)
+	return channel, nil
 }
 func (tc *TestController) ChannelEditComplex(channelID string, data *discordgo.ChannelEdit) (st *discordgo.Channel, err error) {
 	tc.channelEditComplexCallCount++
@@ -50,7 +59,7 @@ func TestGuildCreateSimple(t *testing.T) {
 	controller := &TestController{}
 	bot := newBot(session, controller)
 	guild := &discordgo.Guild{
-		ID: "guild-1",
+		ID: TEST_GUILD_ID,
 	}
 	// GuildID is necessary in archiveThreadsIfNecessary
 	// TODO: maybe find a way to not need it
@@ -59,13 +68,13 @@ func TestGuildCreateSimple(t *testing.T) {
 			ID:      "thread-archive-category",
 			Type:    discordgo.ChannelTypeGuildCategory,
 			Name:    THREAD_ARCHIVE_CATEGORY_NAME + " 72",
-			GuildID: "guild-1",
+			GuildID: TEST_GUILD_ID,
 		},
 		{
 			ID:      "thread-category",
 			Type:    discordgo.ChannelTypeGuildCategory,
 			Name:    THREAD_CATEGORY_NAME,
-			GuildID: "guild-1",
+			GuildID: TEST_GUILD_ID,
 		}}
 	session.State.GuildAdd(guild)
 	bot.guildCreate(session, newGuildCreate((guild)))
@@ -82,63 +91,62 @@ func TestGuildCreateTriggerArchive(t *testing.T) {
 	maxActiveThreads = 5
 	controller := &TestController{}
 	bot := newBot(session, controller)
-	guildID := "guild-1"
 	guild := &discordgo.Guild{
-		ID: guildID,
+		ID: TEST_GUILD_ID,
 	}
 	guild.Channels = []*discordgo.Channel{
 		{
 			ID:      "thread-archive-category",
 			Type:    discordgo.ChannelTypeGuildCategory,
 			Name:    THREAD_ARCHIVE_CATEGORY_NAME + " 72",
-			GuildID: guildID,
+			GuildID: TEST_GUILD_ID,
 		},
 		{
 			ID:      "thread-category",
 			Type:    discordgo.ChannelTypeGuildCategory,
 			Name:    THREAD_CATEGORY_NAME,
-			GuildID: guildID,
+			GuildID: TEST_GUILD_ID,
 		},
 		{
 			ID:       "channel-1",
 			Type:     discordgo.ChannelTypeGuildText,
 			Name:     "channel-1",
-			GuildID:  guildID,
+			GuildID:  TEST_GUILD_ID,
 			ParentID: "thread-category",
 		},
 		{
 			ID:       "channel-2",
 			Type:     discordgo.ChannelTypeGuildText,
 			Name:     "channel-2",
-			GuildID:  guildID,
+			GuildID:  TEST_GUILD_ID,
 			ParentID: "thread-category",
 		},
 		{
 			ID:       "channel-3",
 			Type:     discordgo.ChannelTypeGuildText,
 			Name:     "channel-3",
-			GuildID:  guildID,
+			GuildID:  TEST_GUILD_ID,
 			ParentID: "thread-category",
 		},
 		{
 			ID:       "channel-4",
 			Type:     discordgo.ChannelTypeGuildText,
 			Name:     "channel-4",
-			GuildID:  guildID,
+			GuildID:  TEST_GUILD_ID,
 			ParentID: "thread-category",
 		},
 		{
 			ID:       "channel-5",
 			Type:     discordgo.ChannelTypeGuildText,
 			Name:     "channel-5",
-			GuildID:  guildID,
+			GuildID:  TEST_GUILD_ID,
 			ParentID: "thread-category",
 		},
 		{
 			ID:       "channel-6",
 			Type:     discordgo.ChannelTypeGuildText,
 			Name:     "channel-6",
-			GuildID:  guildID,
+			GuildID:  TEST_GUILD_ID,
 			ParentID: "thread-category",
 		}}
 	session.State.GuildAdd(guild)
@@ -148,5 +156,52 @@ func TestGuildCreateTriggerArchive(t *testing.T) {
 	}
 	if controller.guildChannelCreateComplexCallCount != 0 {
 		t.Errorf("GuildChannelCreateComplex should not have been called.")
+	}
+}
+
+func TestGuildCreateTriggerCreatingArchivedCategory(t *testing.T) {
+	session := newSessionStub()
+	maxActiveThreads = 1
+	controller := &TestController{}
+	controller.session = session
+	bot := newBot(session, controller)
+	guild := &discordgo.Guild{
+		ID: TEST_GUILD_ID,
+	}
+	guild.Roles = []*discordgo.Role{
+		{
+			ID:   "everyone-role",
+			Name: EVERYONE_ROLE_NAME,
+		},
+	}
+	guild.Channels = []*discordgo.Channel{
+		{
+			ID:      "thread-category",
+			Type:    discordgo.ChannelTypeGuildCategory,
+			Name:    THREAD_CATEGORY_NAME,
+			GuildID: TEST_GUILD_ID,
+		},
+		{
+			ID:       "channel-1",
+			Type:     discordgo.ChannelTypeGuildText,
+			Name:     "channel-1",
+			GuildID:  TEST_GUILD_ID,
+			ParentID: "thread-category",
+		},
+		{
+			ID:       "channel-2",
+			Type:     discordgo.ChannelTypeGuildText,
+			Name:     "channel-2",
+			GuildID:  TEST_GUILD_ID,
+			ParentID: "thread-category",
+		},
+	}
+	session.State.GuildAdd(guild)
+	bot.guildCreate(session, newGuildCreate((guild)))
+	if controller.channelEditComplexCallCount != 1 {
+		t.Errorf("ChannelEditComplex should have been called once.")
+	}
+	if controller.guildChannelCreateComplexCallCount != 1 {
+		t.Errorf("GuildChannelCreateComplex should have been called once.")
 	}
 }
