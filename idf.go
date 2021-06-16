jkomoros/flux-bot

commit 02cf58453300ecae626e8e7a747e063950f124bc

Fixed a bug where TFIDF.AutoTopWords would fail if there were even fewer total words seen then were asked for. Part of #34.

idf.go
@@ -244,6 +244,10 @@ func (t *TFIDF) AutoTopWords(maxCount int) []string {
 		fmt.Printf("maxDropIndex: %v, total words: %v\n", maxDropIndex, rawWords[:maxDropIndex])
 	}
 
+	if maxDropIndex < len(rawWords) {
+		maxDropIndex = len(rawWords)
+	}
+
 	return t.restemWords(rawWords[:maxDropIndex])
 }
 

commit fe4a9a6d825a0c1c3b3bc56f777bed048619689b

Fixed an error introduced in 02cf58453300ecae626e8e7a747e063950f124bc
The condition was backwards.

Tests were failing, now are fixed.

idf.go
@@ -244,7 +244,7 @@ func (t *TFIDF) AutoTopWords(maxCount int) []string {
 		fmt.Printf("maxDropIndex: %v, total words: %v\n", maxDropIndex, rawWords[:maxDropIndex])
 	}
 
-	if maxDropIndex < len(rawWords) {
+	if maxDropIndex > len(rawWords) {
 		maxDropIndex = len(rawWords)
 	}
 